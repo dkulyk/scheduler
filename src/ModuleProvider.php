81b353ac79818d6a56590b5f44efaf1663cfa05d
Dmytro Kulyk <dmytro@dkulyk.name>
2019-04-09 14:21:21 +0300

    init

---
+++ src/ModuleProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DKulyk\Scheduler;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class ModuleProvider.
+ */
+final class ModuleProvider extends ServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+
+    }
+}
