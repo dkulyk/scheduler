70579217a607f7bda3b2fcb84d2a210b5b1ed5eb
Dmytro Kulyk <dmytro@dkulyk.name>
2019-10-04 15:54:10 +0300

    Added policy.

---
+++ src/Policy/SchedulePolicy.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DKulyk\Scheduler\Policy;
+
+use DKulyk\Scheduler\Entities\Schedule;
+use DtKt\Nova\Policies\ModelPolicy;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class SchedulePolicy
+ */
+class SchedulePolicy extends ModelPolicy
+{
+
+    /**
+     * @return Model
+     */
+    public static function model(): string
+    {
+        return Schedule::class;
+    }
+}
